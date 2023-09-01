bed4a064466ff421b42edcaf98cd3ecf5c00c96c
akp111 <pradhanashish95@gmail.com>
2023-09-01 19:46:25 +0530

    added a testcase for sign

--- test/unit/ledger/sign.test.ts
+++ test/unit/ledger/sign.test.ts
@@ -1,19 +1,54 @@
-import { sign } from '../../../src/ledger/sign';
-import fs from 'fs';
-jest.mock('fs');
+import { ledgerSign, signId } from '../../../src/ledger/sign';
+import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
+import AvalancheApp from '@avalabs/hw-app-avalanche';
+import { sha256 } from 'ethereumjs-util';
 
+jest.mock('@avalabs/hw-app-avalanche');
+jest.mock('@ledgerhq/hw-transport-node-hid');
+jest.mock('ethereumjs-util');
+describe('ledger/sign testcases', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-describe.skip('ledger/sign testcases', () => {
-  test('should throw an error when unable to read the transaction file', async () => {
-    // Mock file read error
+  it('should successfully sign a blind transaction', async () => {
+    // Mock the necessary dependencies and their responses.
+    const mockTransport = {
+      open: jest.fn().mockResolvedValue({})
+    };
+    const mockAvalanche = {
+      signHash: jest.fn().mockResolvedValue({
+        errorMessage: 'No errors',
+        returnCode: 0,
+        signatures: {
+          get: jest.fn().mockResolvedValue('mocksign')
+        }
+      })
+    };
     //@ts-ignore
-    fs.readFileSync.mockImplementation(() => {
-      throw new Error('File read error');
-    });
+    TransportNodeHid.open.mockResolvedValue(mockTransport);
+    //@ts-ignore
+    AvalancheApp.mockImplementation(() => mockAvalanche);
+    //@ts-ignore
+    sha256.mockReturnValue(Buffer.from('mockedMessageHash', 'hex'));
 
-    const mockDerivationPath = "m/44'/9000'/0'/0/0";
+    const mockTx = {
+      signatureRequests: [{ message: 'mockedMessage' }],
+      unsignedTransactionBuffer: 'mockedUnsignedTxBuffer'
+    };
+    jest
+      .spyOn(require('../../../src/ledger/utils'), 'recoverTransactionPublicKey')
+      .mockReturnValue(
+        '04423fb5371af0e80750a6481bf9b4adcf2cde38786c4e613855b4f629f8c45ded6720e3335d1110c112c6d1c17fcbb23b9acc29ae5750a27637d385991af15190'
+      );
+    jest
+      .spyOn(require('../../../src/ledger/utils'), 'recoverTransactionSigner')
+      .mockReturnValue('0xfa32C77AA014584bB9c3F69d8D1d74B8844e1A92');
+    //@ts-ignore
+    const result = await ledgerSign(mockTx, "m/44'/9000'/0'/0/0", true);
+    expect(result).toHaveProperty('address', '0xfa32C77AA014584bB9c3F69d8D1d74B8844e1A92');
 
-    // Execute the sign function and expect it to throw an error
-    await expect(sign('unsignedTx.json', mockDerivationPath)).rejects.toThrow('File read error');
+    // Verify that the required functions were called.
+    expect(AvalancheApp).toHaveBeenCalled();
   });
 });
